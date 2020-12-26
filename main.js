eb71e74f519c8a98a7ed26d069745d5d15692663
AryaAnish121 <73824605+AryaAnish121@users.noreply.github.com>
2020-12-26 15:06:37 +0530

    My bro's website
    This is my  bro's website

---
+++ main.js
@@ -0,0 +1,17 @@
+var button = document.querySelector('.mybutton');
+
+button.addEventListener('click', function(){
+  window.open('https://youtube.com/channel/UCdOz0n84ViSKJDujNNkqjxQ', "_self");
+});
+
+var button = document.querySelector('.Subscribe');
+
+button.addEventListener('click', function() {
+  window.open('https://youtube.com/channel/UCdOz0n84ViSKJDujNNkqjxQ', "_self");
+});
+
+var button = document.querySelector('.Videos');
+
+button.addEventListener('click', function() {
+  window.open('https://youtube.com/channel/UCdOz0n84ViSKJDujNNkqjxQ', "_self");
+});
